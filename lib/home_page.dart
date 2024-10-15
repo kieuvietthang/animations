58c1615bcc8aee11ecc10122e7b6fe12d63dce3e
kieuvietthang <kieuthang217@gmail.com>
2024-10-15 09:00:49 +0700

    merge dev

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -35,84 +35,80 @@ class _HomePageState extends State<HomePage> {
         ),
         child: Stack(
           children: [
-            Positioned.fill(
-              child: AnimatedPositioned(
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.easeInOut,
-                bottom: _isVisible ? screenSize.height / 2 - 50 : -170,
-                left: 10,
-                right: 10,
-                child: Container(
-                  height: screenSize.height * 0.22,
-                  width: screenSize.width,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  padding: const EdgeInsets.only(left: 15,right: 15),
-                  child: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                        "Xin chào, tôi là Pingo!",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "Hãy để tôi chỉ cho banj biết cách ứng dụng này hoạt động. Cho phép Findmykids theo dõi hoạt động của bạn",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.normal,
-                            fontSize: 14),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ],
-                  ),
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              bottom: _isVisible ? screenSize.height / 2 - 50 : -200,
+              left: 10,
+              right: 10,
+              child: Container(
+                height: screenSize.height * 0.22,
+                width: screenSize.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.only(left: 15,right: 15),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      "Xin chào, tôi là Pingo!",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Hãy để tôi chỉ cho banj biết cách ứng dụng này hoạt động. Cho phép Findmykids theo dõi hoạt động của bạn",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
                 ),
               ),
             ),
-            Positioned.fill(
-              child: AnimatedPositioned(
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.easeInOut,
-                bottom: _isVisible ? 30 : -100,
-                left: 25,
-                right: 25,
-                child: GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      _isVisible = false;
-                    });
-                  },
-                  child: Container(
-                    height: screenSize.height * 0.06,
-                    width: screenSize.width * 0.78,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Colors.blue, borderRadius: BorderRadius.circular(5)),
-                    child: const Text(
-                      "Bắt đầu",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              bottom: _isVisible ? 30 : -100,
+              left: 25,
+              right: 25,
+              child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    _isVisible = false;
+                  });
+                },
+                child: Container(
+                  height: screenSize.height * 0.06,
+                  width: screenSize.width * 0.78,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.blue, borderRadius: BorderRadius.circular(5)),
+                  child: const Text(
+                    "Bắt đầu",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
